acc5af6ae7624865703f24c249257de40fc06622
hasibarrafiul <fahad.com66.fk@gmail.com>
2022-11-27 00:17:07 +0600

    car rotation added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme/index';
 import { ColorModeSwitcher } from './theme/ColorModeSwitcher';
 import React, { useEffect, useState } from "react";
-import car from './asset/car.png';
+import car1_up from './asset/car1up.png';
+import car1_down from './asset/car1down.png';
+import car1_left from './asset/car1left.png';
+import car1_right from './asset/car1right.png';
 
 
 function App() {
@@ -10,19 +13,24 @@ function App() {
   const [xoffset, setXoffset] = useState(500);
   const [yoffset, setYoffset] = useState(700);
   const [delta, setDelta] = useState(10);
+  const [car1, setCar1] = useState(car1_up);
 
   useEffect(() => {
     const handleEsc = (event) => {
        if (event.keyCode === 87) {
+        setCar1(car1_up);
         setYoffset(yoffset-delta);
       }
       if (event.keyCode === 65) {
+        setCar1(car1_left);
         setXoffset(xoffset-delta);
       }
       if (event.keyCode === 83) {
+        setCar1(car1_down);
         setYoffset(yoffset+delta);
       }
       if (event.keyCode === 68) {
+        setCar1(car1_right);
         setXoffset(xoffset+delta);
       }
     };
@@ -54,7 +62,7 @@ function App() {
 			top: `${yoffset}px`,
 		}}
 		>
-		<img src={car} height="100" width="100"/>
+		<img src={car1} height="250" width="250"/>
 		</h2>
     </ChakraProvider>
   );
