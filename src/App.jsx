4bff9ff0bc28dfa623b76fa23e1da712660cca38
hasibarrafiul <fahad.com66.fk@gmail.com>
2022-11-26 23:57:26 +0600

    car and movement added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from './theme/index';
 import { ColorModeSwitcher } from './theme/ColorModeSwitcher';
+import React, { useEffect, useState } from "react";
+import car from './asset/car.png';
 
 
 function App() {
+
+  const [xoffset, setXoffset] = useState(500);
+  const [yoffset, setYoffset] = useState(700);
+  const [delta, setDelta] = useState(10);
+
+  useEffect(() => {
+    const handleEsc = (event) => {
+       if (event.keyCode === 87) {
+        setYoffset(yoffset-delta);
+      }
+      if (event.keyCode === 65) {
+        setXoffset(xoffset-delta);
+      }
+      if (event.keyCode === 83) {
+        setYoffset(yoffset+delta);
+      }
+      if (event.keyCode === 68) {
+        setXoffset(xoffset+delta);
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [xoffset, yoffset]);
+
+  
   return (
     <ChakraProvider theme={theme}>
       <ColorModeSwitcher
@@ -16,6 +45,17 @@ function App() {
         }}
       />
      <h1>extreme riders</h1>
+
+     
+     <h2
+		style={{
+			position: "absolute",
+			left: `${xoffset}px`,
+			top: `${yoffset}px`,
+		}}
+		>
+		<img src={car} height="100" width="100"/>
+		</h2>
     </ChakraProvider>
   );
 }
